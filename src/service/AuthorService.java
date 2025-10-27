721f6217fd1278b39d0e762a8e2edf03266cc1fb
mageshkannan <mageshkannanam@gmail.com>
2025-10-27 17:55:51 +0530

    fix: add app user service with role id

--- src/service/AuthorService.java
+++ src/service/AuthorService.java
@@ -15,6 +15,7 @@ public class AuthorService {
     }
 
     public int addAuthor(Author author) throws SQLException {
+        Objects.requireNonNull(author);
         validateAuthor(author);
         Author existingAuthor = authorDAO.getAuthorByName(author.getName());
         if (existingAuthor != null) {
@@ -23,6 +24,34 @@ public class AuthorService {
         int author_id = authorDAO.createAuthor(author);
         return author_id;
 
+    }
+
+    public Author getAuthorById(int authorId) throws SQLException {
+        return authorDAO.getAuthorById(authorId);
+    }
+
+    public java.util.List<Author> getAllAuthors() throws SQLException {
+        return authorDAO.getAllAuthors();
+    }
+
+    public Author getAuthorByName(String name) throws SQLException {
+        Validation.requireNonEmpty(name, "Name");
+        return authorDAO.getAuthorByName(name);
+    }
+
+    public java.util.List<Author> searchAuthorsByName(String name) throws SQLException {
+        Validation.requireNonEmpty(name, "Name");
+        return authorDAO.searchAuthorsByName(name);
+    }
+
+    public boolean updateAuthor(Author author) throws SQLException {
+        Objects.requireNonNull(author);
+        validateAuthor(author);
+        return authorDAO.updateAuthor(author);
+    }
+
+    public boolean deleteAuthor(int authorId) throws SQLException {
+        return authorDAO.deleteAuthor(authorId);
     }
 
     private void validateAuthor(Author author) {
